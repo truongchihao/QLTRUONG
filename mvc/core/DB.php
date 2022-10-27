007f812372dffe6184672248eb8cccac4384ecb2
Hao <chihao2001lop12t2@gmail.com>
2022-10-27 21:02:46 +0700

    host

--- mvc/core/DB.php
+++ mvc/core/DB.php
@@ -2,9 +2,9 @@
 class DB{
     public $con;
     public $servername = "localhost";
-    public $username = "root";
-    public $password = "";
-    public $dbname = "qltruong";
+    public $username = "id17641207_admin";
+    public $password = "f#G1[>g8T#x_K]<b";
+    public $dbname = "id17641207_db_qltruong";
 
     function __construct(){
         $this->con = mysqli_connect($this->servername, $this->username, $this->password);
